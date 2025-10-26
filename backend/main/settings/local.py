1ef895ee740768e65a1fb286b98e6975aa4e17b4
nkhan364 <naveedkhan13041998@gmail.com>
2025-10-26 09:54:27 -0400

    fix: update local settings for CORS and host configuration

--- backend/main/settings/local.py
+++ backend/main/settings/local.py
@@ -15,7 +15,8 @@ SECRET_KEY = "django-insecure-3)jkjc-9$076kt^q9_b)bl2^$p36%%#zc4%^0c@sg_d17ch_6_
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-HOST = "localhost"
+# Allows us to access the vite app over the network (e.g., from a mobile device on the same WiFi)
+HOST = "*"
 
 # Local URLs
 MAIN_URL = "http://localhost:5000/"
@@ -23,17 +24,19 @@ MAIN_URL_2 = "http://localhost:5000"
 
 # CORS settings for development
 CSRF_COOKIE_SECURE = False
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:3000",
-]
+# allow all cors in local dev
+CORS_ALLOW_ALL_ORIGINS = True
+# CSRF_TRUSTED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:8000",
+#     "http://localhost:3000",
+# ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:8000",
+#     "http://localhost:3000",
+# ]
 
 X_FRAME_OPTIONS = "ALLOW-FROM http://localhost:3000/"
 
